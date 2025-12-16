c362734f62b412293a59f2803c4e69bfb5d7aced
Duanetje <duane.roe@connect.be>
2025-12-16 14:49:57 +0100

    lighting aangepast

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-useGLTF.preload("2025-12-12_Lantis-OWV_Uitkijktoren-2.glb");
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, useGLTF, Bounds, Center } from "@react-three/drei";
+import { OrbitControls, useGLTF, Bounds, Center, Environment } from "@react-three/drei";
 import "./App.css";
+
+useGLTF.preload("2025-12-12_Lantis-OWV_Uitkijktoren-2.glb");
 
 function Uitkijktoren() {
   const gltf = useGLTF("/2025-12-12_Lantis-OWV_Uitkijktoren-2.glb"); // werkt via public folder
@@ -17,9 +18,10 @@ function App() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <Canvas camera={{ position: [3, 3, 5], fov: 50 }}>
-        <ambientLight intensity={1} />
-        <directionalLight position={[10, 10, 5]} intensity={2} />
-        <directionalLight position={[-10, -10, 5]} intensity={2} helper />
+        <ambientLight intensity={0.2} />
+        <directionalLight position={[6, 10, 5]} intensity={1.2} />
+        <directionalLight position={[-4, 5, -6]} intensity={0.6} />
+        <Environment files="/sunny_rose_garden_2k.hdr" background={false} />
         <Bounds fit clip observe margin={1.2}>
           <Uitkijktoren />
         </Bounds>
